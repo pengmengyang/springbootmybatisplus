eb563b8d3c884a99494c76ed5e2a63cb02dd5dad
646410289 <646410289@qq.com>
2022-04-19 23:52:57 +0800

    新增查询接口以及分页查询

---
+++ src/main/java/com/example/springbootmybatisplus/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.example.springbootmybatisplus.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 64641
+ * <p>
+ * 分页插件配置
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor getMybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MARIADB));
+        return mybatisPlusInterceptor;
+    }
+
+}
--- src/main/java/com/example/springbootmybatisplus/user/controller/UserController.java
+++ src/main/java/com/example/springbootmybatisplus/user/controller/UserController.java
@@ -1,18 +1,69 @@
 package com.example.springbootmybatisplus.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.springbootmybatisplus.user.service.IUserService;
+import com.example.springbootmybatisplus.user.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 6464
  * @since 2022-04-18
  */
 @RestController
-@RequestMapping("/user/user-vo")
+@Slf4j
+@RequestMapping("/user")
 public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/getUser")
+    public UserVO getUser() {
+        UserVO userVO = userService.getById(1);
+        log.info("----getUser----: {}", userVO.toString());
+        return userVO;
+    }
+
+    @GetMapping("/getAllUser")
+    public List<UserVO> getAllUser() {
+        List<UserVO> list = userService.list();
+        for (UserVO userVO : list) {
+            log.info("---getAllUser----: {}", userVO.toString());
+        }
+        return list;
+    }
+
+    @GetMapping("/getUserByParam")
+    public List<UserVO> getUserByParam(@RequestParam("age") String age, @RequestParam("name") String name) {
+        QueryWrapper<UserVO> queryWrapper = new QueryWrapper();
+        queryWrapper.ge("age", age);
+        queryWrapper.eq("name", name);
+        List<UserVO> list = userService.list(queryWrapper);
+        for (UserVO userVO : list) {
+            log.info("---getUserByParam----: {}", userVO.toString());
+        }
+        return list;
+    }
+
+    @GetMapping("/getPageUser/{size}/{current}")
+    public Page<UserVO> getPageUser(@PathVariable("current") Integer current, @PathVariable("size") Integer size) {
+        Page<UserVO> page = new Page<>(current, size);
+        Page<UserVO> userVOPage = userService.page(page);
+        return userVOPage;
+    }
 
 }
